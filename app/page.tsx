f9d385f102a70b891b649b56c82e1c8655173230
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-11-15 10:10:15 -0600

    set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
-import { Metadata } from "next";
+import ECommerce from '@/components/Dashboard/E-commerce';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "TailAdmin | Next.js E-commerce Dashboard Template",
-  description: "This is Home Blog page for TailAdmin Next.js",
-  // other metadata
+	title: 'Dashboard Admin',
+	description: 'An example of Terraform, Ansible and Nextjs working together',
+	// other metadata
 };
 
 export default function Home() {
-  return (
-    <>
-      <ECommerce />
-    </>
-  );
+	return (
+		<>
+			<ECommerce />
+		</>
+	);
 }
--- components/Charts/ChartTwo.tsx
+++ components/Charts/ChartTwo.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const options: ApexOptions = {
-  colors: ["#3C50E0", "#80CAEE"],
+  colors: ["#BE3144", "#F05941"],
   chart: {
     // events: {
     //   beforeMount: (chart) => {
@@ -98,7 +98,7 @@ const ChartTwo: React.FC = () => {
   handleReset;
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+    <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-7">
       <div className="mb-4 justify-between gap-4 sm:flex">
         <div>
           <h4 className="text-xl font-semibold text-black dark:text-white">
